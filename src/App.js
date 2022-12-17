7474aec28a1229f783f44e3614eacaa28a7342b6
BuildTools <unconfigured@null.spigotmc.org>
2022-12-17 14:38:53 -0700

    Compeleted my first use of React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  let [counter, setCounter] = useState(1)
+  window.setCounter = setCounter
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
